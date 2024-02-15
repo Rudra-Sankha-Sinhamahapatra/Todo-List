6dd76e599ffcd5566ff789ca26f7be1a910b2a75
Rudra Sankha Sinhamahapatra <101992909+Rudra-Sankha-Sinhamahapatra@users.noreply.github.com>
2024-02-16 01:17:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,6 @@ const addTodo = (todo)=>{
 setTodos((prev)=>[{id:Date.now(),...todo},...prev])
 }
 
-//looping each element by map method
-//todos is an array so setTodos set array elements
-//if id matches with the passing id then todo otherwise stay with the previous todo
-
-// other technique
-// prev.map((eachVal)=>{
-  // if(eachVal.id===id){
-  //   todo
-  // }
-  // else{
-  //   prevTodo
-  // }
-  // });
 
 const updatedTodo=(id,todo)=>{
 setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id===id ? todo : prevTodo)));
@@ -53,11 +40,11 @@ localStorage.setItem("todos",JSON.stringify(todos));
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        
                       <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
-                        {/*Loop and Add TodoItem here */}
+                   
                         {todos.map((todo)=>(
                           <div key={todo.id}
                           className='w-full'>
